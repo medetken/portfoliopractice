34f6444961733e78e52e755cb5814a3a47aeee14
Medet Kenenbayev <medetkenenbayev@Medets-MacBook-Pro.local>
2024-03-27 13:15:53 -0400

    Create a nav button and page background

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("welcome to my site");
+
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.getElementById("ribbonMenu").classList.toggle("active");
+});
